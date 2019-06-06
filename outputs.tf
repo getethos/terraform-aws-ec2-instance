cb17b8bf934268c2d0aa5f28d6c52d2278e58a52
Tim <4141828+heisian@users.noreply.github.com>
2019-06-05 17:15:06 -0700

    Update outputs.tf
    unnecessarily wrapped lists in a list

--- outputs.tf
+++ outputs.tf
@@ -118,12 +118,12 @@ locals {
 
 output "id" {
   description = "List of IDs of instances"
-  value       = [local.this_id]
+  value       = local.this_id
 }
 
 output "availability_zone" {
   description = "List of availability zones of instances"
-  value       = [local.this_availability_zone]
+  value       = local.this_availability_zone
 }
 
 // GH issue: https://github.com/terraform-aws-modules/terraform-aws-ec2-instance/issues/8
@@ -134,56 +134,56 @@ output "availability_zone" {
 
 output "key_name" {
   description = "List of key names of instances"
-  value       = [local.this_key_name]
+  value       = local.this_key_name
 }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
-  value       = [local.this_public_dns]
+  value       = local.this_public_dns
 }
 
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
-  value       = [local.this_public_ip]
+  value       = local.this_public_ip
 }
 
 output "primary_network_interface_id" {
   description = "List of IDs of the primary network interface of instances"
-  value       = [local.this_primary_network_interface_id]
+  value       = local.this_primary_network_interface_id
 }
 
 output "private_dns" {
   description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       = [local.this_private_dns]
+  value       = local.this_private_dns
 }
 
 output "private_ip" {
   description = "List of private IP addresses assigned to the instances"
-  value       = [local.this_private_ip]
+  value       = local.this_private_ip
 }
 
 output "security_groups" {
   description = "List of associated security groups of instances"
-  value       = [local.this_security_groups]
+  value       = local.this_security_groups
 }
 
 output "vpc_security_group_ids" {
   description = "List of associated security groups of instances, if running in non-default VPC"
-  value       = [local.this_vpc_security_group_ids]
+  value       = local.this_vpc_security_group_ids
 }
 
 output "subnet_id" {
   description = "List of IDs of VPC subnets of instances"
-  value       = [local.this_subnet_id]
+  value       = local.this_subnet_id
 }
 
 output "credit_specification" {
   description = "List of credit specification of instances"
-  value       = [local.this_credit_specification]
+  value       = local.this_credit_specification
 }
 
 output "tags" {
   description = "List of tags of instances"
-  value       = [local.this_tags]
+  value       = local.this_tags
 }
 
